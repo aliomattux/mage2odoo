86d5a406b767d2dbfd27c3a8b834d10c863b5820
aliomattux <kyle.waid@gcotech.com>
2014-12-17 13:22:00 -0600

    Remove purchase fields in this module

--- models/procurement.py
+++ models/procurement.py
@@ -1,31 +1,6 @@
 from openerp import SUPERUSER_ID, api
 from openerp.osv import osv, fields
 
-class PurchaseOrder(osv.osv):
-    _inherit = 'purchase.order'
-
-
-    def _prepare_purchase_picking_vals(self, cr, uid, purchase, context=None):
-        vals = {
-            'picking_type_id': purchase.picking_type_id.id,
-            'partner_id': purchase.dest_address_id.id or purchase.partner_id.id,
-            'date': max([l.date_planned for l in purchase.order_line]),
-            'origin': purchase.name,
-            'purchase': purchase.id
-        }
-
-        return vals
-
-    #This method is overridden because there is no way I know of to add to the vals of a picking record when it is created
-    #There are lots of reasons to add something to a stock.picking if it is created from a Purchase order
-    #IMO a proper solution would be to put values = {} into a prepare vals method so a developer can call super
-    #to add to the vals without having to override a core method
-    def action_picking_create(self, cr, uid, ids, context=None):
-        for order in self.browse(cr, uid, ids):
-	    vals = self._prepare_purchase_picking_vals(cr, uid, order)
-            picking_id = self.pool.get('stock.picking').create(cr, uid, vals, context=context)
-            self._create_stock_moves(cr, uid, order, order.order_line, picking_id, context=context)
-
 
 class StockMove(osv.osv):
     _inherit = 'stock.move'
--- models/stock.py
+++ models/stock.py
@@ -6,7 +6,6 @@ class StockPicking(osv.osv):
 	'mage_export_error': fields.boolean('Magento Export Error'),
 	'external_id': fields.integer('Magento Shipment ID'),
         'sale': fields.many2one('sale.order', 'Sale Order'),
-	'purchase': fields.many2one('purchase.order', 'Purchase Order'),
         'shipping_state': fields.related('partner_id', 'state_id', type='many2one', relation='res.country.state', string='Shipping State'),
         'shipping_city': fields.related('partner_id', 'city', type='char', string='Shipping City'),
         'mage_store': fields.related('sale', 'mage_store', type='many2one', relation='mage.store.view', string='Magento Store'),
