b04ef44b52aa2fa8c4aacf882340d73c233f81f5
aliomattux <kyle.waid@gcotech.com>
2014-12-18 16:02:29 -0600

    Call super instead of override. This was causing problem in another customization module This will reduce performance as it does a second write, but was necessary

--- models/procurement.py
+++ models/procurement.py
@@ -16,41 +16,21 @@ class StockMove(osv.osv):
 
 	return vals
 
-    def _prepare_picking_assign_vals(self, cr, uid, move, context=None):
-        vals = {
-                'origin': move.origin,
-                'company_id': move.company_id and move.company_id.id or False,
-                'move_type': move.group_id and move.group_id.move_type or 'direct',
-                'partner_id': move.partner_id.id or False,
-                'picking_type_id': move.picking_type_id and move.picking_type_id.id or False,
-        }
 
-
-	if move.procurement_id.sale_line_id:
-	    vals.update(self.add_sale_vals(cr, uid, move.procurement_id.sale_line_id.order_id))
-
-        return vals
-
-    #This method is overridden because there is no way I know of to add to the vals of a picking record when it is created
-    #There are lots of reasons to add something to a stock.picking if it is created from a sales order > procurement
-    #IMO a proper solution would be to put values = {} into a prepare vals method so a developer can call super
-    #to add to the vals without having to override a core method
     @api.cr_uid_ids_context
-    def _picking_assign(self, cr, uid, move_ids, procurement_group, location_from, location_to, context=None):
-        """Assign a picking on the given move_ids, which is a list of move supposed to share the same procurement_group, location_from and location_to
-        (and company). Those attributes are also given as parameters.
-        """
-        pick_obj = self.pool.get("stock.picking")
-        picks = pick_obj.search(cr, uid, [
-                ('group_id', '=', procurement_group),
-                ('location_id', '=', location_from),
-                ('location_dest_id', '=', location_to),
-                ('state', 'in', ['draft', 'confirmed', 'waiting'])], context=context)
+    def _picking_assign(self, cr, uid, move_ids, procurement_group, \
+                location_from, location_to, context=None):
 
-        if picks:
-            pick = picks[0]
-        else:
-            move = self.browse(cr, uid, move_ids, context=context)[0]
-	    vals = self._prepare_picking_assign_vals(cr, uid, move)
-            pick = pick_obj.create(cr, uid, vals, context=context)
-        return self.write(cr, uid, move_ids, {'picking_id': pick}, context=context)
+        picking_obj = self.pool.get('stock.picking')
+
+        res = super(StockMove, self)._picking_assign(cr, uid, move_ids, procurement_group, \
+                location_from, location_to, context=context)
+
+        moves = self.browse(cr, uid, move_ids)
+
+        if moves[0].procurement_id.sale_line_id:
+	    picking = moves[0].picking_id
+	    vals = self.add_sale_vals(cr, uid, moves[0].procurement_id.sale_line_id.order_id)
+            picking_obj.write(cr, uid, picking.id, vals)
+
+        return res
