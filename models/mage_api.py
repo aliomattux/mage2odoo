543c907d95c43571a6dd7b16a84b1b4b46993514
root <root@odoo.htm-mbs.com>
2015-09-06 04:27:42 -0400

    Add easy support for making external ad hoc call. This needs to be revisited from a security perspective

--- models/mage_api.py
+++ models/mage_api.py
@@ -17,6 +17,20 @@ import xmlrpclib
 class MageIntegrator(osv.osv):
     _name = 'mage.integrator'
 
+    def get_external_credentials(self):
+	mage_obj = self.pool.get('mage.setup')
+	setup_ids = mage_obj.search(cr, uid, [], limit=1)
+	if setup_ids:
+	    instance = setup_ids.browse(cr, uid, setup_ids[0]
+	    return {
+		'url': instance.url,
+		'username': instance.username,
+		'password': instance.password,
+	    }
+	else:
+	    raise osv.except_osv(_('Config Error'), _('You must have already configured Magento to run this function!'))
+	    
+
     def _get_credentials(self, job):
         return {
                 'url': job.mage_instance.url,
@@ -40,24 +54,17 @@ class MageIntegrator(osv.osv):
 
         except (socket.gaierror, socket.error, socket.timeout) as err:
 	    print 'Network Error'
-#            raise NetworkRetryableError(
- #               'A network error caused the failure of the job: '
-  #              '%s' % err)
         except xmlrpclib.ProtocolError as err:
             if err.errcode in [502,   # Bad gateway
                                503,   # Service unavailable
                                504]:  # Gateway timeout
-	        print 'Other Error'
-#                raise RetryableJobError(
- #                   'A protocol error caused the failure of the job:\n'
-  #                  'URL: %s\n'
-   #                 'HTTP/HTTPS headers: %s\n'
-    #                'Error code: %d\n'
-     #               'Error message: %s\n' %
-      #              (err.url, err.headers, err.errcode, err.errmsg))
+		raise osv.except_osv(_('Connection Error'), _('Server Returned Error Code: %s')%err.errcode)
             else:
-                raise
-
+		raise osv.except_osv(_('Generic Connection Error'), _('Could Not Authenticate/Connect with Magento!\n
+Please verify that\n1. The Magento module is installed in the LOCAL folder on Magento.\n
+2. Your webservice user in Magento is setup with FULL Admin access.\n
+3. Verify your Odoo server can reach Magento.\n
+4. Verify that you have entered the correct Credentials'))
 
     def _get_product_type_selections(self, cr, uid, job, conversion_type, \
 		mappingline, record):
