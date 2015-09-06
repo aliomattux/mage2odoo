e9fe065526abd211c6eced7073cb89407b88ca17
root <root@odoo.htm-mbs.com>
2015-09-06 05:52:55 -0400

    Add updated partner sync, fix bug

--- jobs/partner.py
+++ jobs/partner.py
@@ -8,11 +8,19 @@ class MageIntegrator(osv.osv_memory):
     _inherit = 'mage.integrator'
 
 
-    def import_all_partners(self, cr, uid, job, context=None):
+    def import_updated_partners(self, cr, uid, job, context=None):
 	instance = job.mage_instance
-	filters = False
+	filters = {}
 	if instance.last_imported_customer:
 	    filters = {'entity_id': {'gt': instance.last_imported_customer}}
+
+
+        tz = get_localzone()
+        now = datetime.utcnow()
+        dt = tz.localize(now)
+        string = dt.strftime('%Y-%m-%d')
+        updated = {'updated_at': {'gteq': string}}
+	filters.update(updated)
 
         partner_ids = self._get_job_data(cr, uid, job, 'oo_customer.search', [filters])
 
@@ -20,6 +28,20 @@ class MageIntegrator(osv.osv_memory):
 	    return True
 
 	return self.import_partners(cr, uid, job, partner_ids)
+
+
+    def import_all_partners(self, cr, uid, job, context=None):
+        instance = job.mage_instance
+        filters = False
+        if instance.last_imported_customer:
+            filters = {'entity_id': {'gt': instance.last_imported_customer}}
+
+        partner_ids = self._get_job_data(cr, uid, job, 'oo_customer.search', [filters])
+
+        if not partner_ids:
+            return True
+
+        return self.import_partners(cr, uid, job, partner_ids)
 
 
     def import_partners(self, cr, uid, job, partner_ids, context=None):
@@ -43,9 +65,9 @@ class MageIntegrator(osv.osv_memory):
 
 	        if record['addresses']:
 		    for address in record['addresses']:
-		        partner_obj.get_or_create_partner_address(cr, uid, address, partner)
+			pp(address)
+		        partner_obj.get_or_create_partner_address(cr, uid, address, partner, address_type=False)
                         print 'Synced Customer Address'
 	    except Exception, e:
 		print e
 	return True
-        
